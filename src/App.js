0cf0639e2dd5e8cd4dd15a18ca20832b546cf824
wjabbour <wjabbour@memphis.edu>
2020-01-27 14:49:00 -0600

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+class App extends Component {
+  render() {
+    return (
+      <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Steve Jobs</h5>
+        <h6 class="card-subtitle mb-2 text-muted">steve@apple.com</h6>
+        <p class="card-text">Stay Huangry, Stay Foolish</p>
+      </div>
     </div>
-  );
+    );
+  }
+  
 }
 
 export default App;
