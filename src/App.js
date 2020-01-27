be13a145fd997fb4cae13d6f8dbe56fa01adc773
wjabbour <wjabbour@memphis.edu>
2020-01-27 14:59:08 -0600

    Create framework of the application

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import WatchTable from './components/WatchTable';
+import CreateWatch from './components/CreateWatch';
 
 
 
 class App extends Component {
   render() {
     return (
-      <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Steve Jobs</h5>
-        <h6 class="card-subtitle mb-2 text-muted">steve@apple.com</h6>
-        <p class="card-text">Stay Huangry, Stay Foolish</p>
+      <div id="App">
+        <WatchTable />
+        <CreateWatch />
       </div>
-    </div>
     );
   }
   
---
+++ src/components/CreateWatch.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+class CreateWatch extends Component {
+    render() {
+        return <p>My CreateWatch!</p>
+    }
+}
+
+export default CreateWatch;
