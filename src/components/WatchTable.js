1c337c7c269f328546b0f6f5f9ab2ceef8e3850a
wjabbour <wjabbour@memphis.edu>
2020-01-27 17:25:32 -0600

    Add http-service, getWatches stub

--- src/components/WatchTable.js
+++ src/components/WatchTable.js
@@ -1,7 +1,13 @@
-import React, { Component } from 'react';
-
+import React, { Component, props } from 'react';
+import {getWatches} from '../services/http-service';
 
 class WatchTable extends Component {
+    constructor() {
+        super(props);
+    }
+    componentDidMount() {
+        getWatches();
+    }
     render() {
         return <p>My Table!</p>
     }
---
+++ src/services/http-service.js
@@ -0,0 +1,6 @@
+
+export function getWatches() {
+    console.log("Got some watches!");
+}
+
+
