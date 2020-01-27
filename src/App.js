217bbc6d7b89c598e957bb2e5782c8a9f816ee67
wjabbour <wjabbour@memphis.edu>
2020-01-27 15:11:47 -0600

    Create navbar and navbar functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import WatchTable from './components/WatchTable';
 import CreateWatch from './components/CreateWatch';
 
@@ -8,10 +9,26 @@ import CreateWatch from './components/CreateWatch';
 class App extends Component {
   render() {
     return (
-      <div id="App">
-        <WatchTable />
-        <CreateWatch />
-      </div>
+      <Router>
+        <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to="/" className="navbar-brand">Market Watcher</Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">myWatch</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">createWatch</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br/>
+          <Route path="/" exact component={WatchTable} />
+          <Route path="/create" component={CreateWatch} />
+        </div>
+      </Router>
     );
   }
   
