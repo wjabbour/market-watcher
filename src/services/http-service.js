de5f4facaa6d9458b27db3fd4ef2da32776d9c2b
wjabbour <wjabbour@memphis.edu>
2020-01-27 18:41:32 -0600

    Add mock JSON data to getWatch() stub in http-service

--- src/services/http-service.js
+++ src/services/http-service.js
@@ -1,6 +1,14 @@
 
 export function getWatches() {
+    let watch = {
+        "ticker": "AMD",
+        "priceInitiated": "6.00",
+        "currentPrice": "48.50",
+        "dateCreated": "12/1/2017",
+        "percentChange": "300"
+    }
     console.log("Got some watches!");
+    return watch;
 }
 
 export function createWatch() {
