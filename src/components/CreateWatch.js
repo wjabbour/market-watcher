5543b38cbb74bda8cf373d21f5428421be6dd6be
wjabbour <wjabbour@memphis.edu>
2020-02-02 15:43:46 -0600

    Implement basic functionality for CreateWatch

--- src/components/CreateWatch.js
+++ src/components/CreateWatch.js
@@ -2,6 +2,39 @@ import React, { Component } from 'react';
 
 
 class CreateWatch extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            ticker: "",
+            price: 0
+        }
+
+        this.onChangePrice = this.onChangePrice.bind(this);
+        this.onChangeTicker = this.onChangeTicker.bind(this);
+    }
+
+    onChangeTicker(e) {
+        this.setState({
+            ticker: e.target.value
+        });
+    }
+    onChangePrice(e) {
+        this.setState({
+            price: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        console.log("Form is submitted!");
+
+        this.setState({
+            ticker: "",
+            price: 0
+        });
+    }
     render() {
         return <p>My CreateWatch!</p>
     }
