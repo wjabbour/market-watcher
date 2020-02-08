ded0f440b77d27b388a3080e7fd1d18bf26bcf8b
wjabbour <doubleujabbour@gmail.com>
2020-02-08 15:19:19 -0600

    Create watch template

---
+++ src/components/Watch.js
@@ -0,0 +1,16 @@
+import React, { Component, props } from 'react';
+
+class Watch extends Component {
+    constructor() {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                This is a watch!
+            </div>
+        );  
+    }
+}
+
+export default Watch;
