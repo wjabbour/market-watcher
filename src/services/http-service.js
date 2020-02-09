afd7efc64c28a0cff697db259e0e5229f53d2de9
wjabbour <doubleujabbour@gmail.com>
2020-02-09 11:37:52 -0600

    Change parse function for http service

--- src/services/http-service.js
+++ src/services/http-service.js
@@ -2,9 +2,15 @@
 export function getWatches() {
 
     return fetch("http://localhost:3339/watch")
-    .then(res => res.json())
-        .then(result => result);
- 
+        .then(res => res.json())
+}
+
+export function deleteWatch() {
+    return fetch("http://localhost:3339/watch/", {
+        method: 'DELETE'
+    })
+        .then(res => res.json())
+            .then(result => result)
 }
 
 export function createWatch() {
