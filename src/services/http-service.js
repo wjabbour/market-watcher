542ae84c88084489eb9926e889bd165e2ac85338
wjabbour <doubleujabbour@gmail.com>
2020-02-09 13:33:25 -0600

    Remove space for http service

--- src/services/http-service.js
+++ src/services/http-service.js
@@ -1,10 +1,8 @@
 
 export function getWatches() {
-
     return fetch("http://localhost:3339/watch")
         .then(res => res.json())
 }
-
 export function deleteWatch(watchId) {
     return fetch("http://localhost:3339/watch?watchId=" + watchId, {
         method: 'DELETE'
@@ -12,9 +10,6 @@ export function deleteWatch(watchId) {
         // .then(res => res.json())
         //     .then(result => result)
 }
-
 export function createWatch() {
     console.log("Watch created!");
 }
-
-
