96a73f2cdc60ce21be90ce7ad245a6e593f199f0
wjabbour <doubleujabbour@gmail.com>
2020-02-08 19:42:12 -0600

    Change endpoint reference by http service

--- src/services/http-service.js
+++ src/services/http-service.js
@@ -1,7 +1,7 @@
 
 export function getWatches() {
 
-    return fetch("http://localhost:3338/watch")
+    return fetch("http://localhost:3339/watch")
     .then(res => res.json())
         .then(result => result);
  
