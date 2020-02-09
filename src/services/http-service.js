b469a64515203818046b758dcb8b4c523b14ee69
wjabbour <doubleujabbour@gmail.com>
2020-02-08 18:19:09 -0600

    Call express app for json inside http service

--- src/services/http-service.js
+++ src/services/http-service.js
@@ -1,14 +1,10 @@
 
 export function getWatches() {
-    let watch = {
-        "ticker": "AMD",
-        "priceInitiated": "6.00",
-        "currentPrice": "48.50",
-        "dateCreated": "12/1/2017",
-        "percentChange": "300"
-    }
-    console.log("Got some watches!");
-    return watch;
+
+    return fetch("http://localhost:3338/watch")
+    .then(res => res.json())
+        .then(result => result);
+ 
 }
 
 export function createWatch() {
