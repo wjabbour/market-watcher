759751a0486e2ae52bba2c4b81cb2e3829a0ff3d
wjabbour <doubleujabbour@gmail.com>
2020-02-09 11:38:34 -0600

    Add delete button to Watch

--- src/components/Watch.js
+++ src/components/Watch.js
@@ -3,6 +3,12 @@ import React, { Component, props } from 'react';
 class Watch extends Component {
     constructor() {
         super(props);
+
+        this.handleDeleteClick = this.handleDeleteClick.bind(this);
+    }
+    handleDeleteClick(e) {
+        e.preventDefault();
+        this.props.handleDeleteClick(e, this.props.key);
     }
     render() {
         // TODO: some of these props will need to be calculated in the constructor or lifecycle method
@@ -13,6 +19,7 @@ class Watch extends Component {
                 <td>{this.props.currentPrice}</td>
                 <td>{this.props.dateCreated}</td>
                 <td>{this.props.percentChange}</td>
+                <td><button onClick={this.props.handleDeleteClick}>Delete</button></td>
             </tr>
         );  
     }
